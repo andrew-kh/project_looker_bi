240a8689821f4dd5e0ae82e67ae4f310698e6fd7
andrew-kh <andrey.khuhlin@gmail.com>
2023-04-20 08:11:11 +0300

    upd test upload script

--- python/add_test_data_to_pg.py
+++ python/add_test_data_to_pg.py
@@ -1,17 +1,20 @@
 import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
 from get_env import get_env_data_as_dict
 
 env = get_env_data_as_dict('../.env')
 
-connection = psycopg2.connect(
-	user = env['PG_USER'],
-	password = env['PG_PWD'],
-	host = '127.0.0.1',
-	port = '5432',
-	database = 'hh_data'
-)
+engine = create_engine(f'postgresql+psycopg2://{env["PG_USER"]}:{env["PG_PWD"]}@127.0.0.1/hh_data')
 
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM fkb_dwh_ml.t_settings_main;")
-record = cursor.fetchone()
-print(record)
+df = pd.read_csv('/usr/project_looker_bi/misc/hh_test_data.csv', sep='\t')
+
+df = df.rename(columns={
+    'name':'cluster_name',
+    'url':'cluster_url',
+    'count':'cluster_count',
+    'request_dt':'request_time',
+    'attribute':'attribute_name',
+    })
+
+df.to_sql('clusters', con=engine, if_exists='append', schema='dev', index=False)
